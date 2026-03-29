4286cd34ff6ca6d594f420cf09c1f573776d58b6
khanhvv <khanhvv.os@mbbank.com.vn>
2026-03-29 12:43:52 +0700

    Initial commit - project structure organized

---
+++ shopapp-backend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO roles (name) VALUES ('USER');
+INSERT INTO roles (name) VALUES ('ADMIN');
+
+INSERT INTO categories (name) VALUES ('Đồ Điện Tử');
+INSERT INTO categories (name) VALUES ('Thời Trang');
+INSERT INTO categories (name) VALUES ('Đồ Gia Dụng');
+INSERT INTO categories (name) VALUES ('Sách & Văn Phòng Phẩm');
