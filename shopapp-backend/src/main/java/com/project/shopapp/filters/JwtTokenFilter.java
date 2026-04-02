e6026fac01d01c2c5bf8ff5fab4173e5f5a50b43
khanhvv <khanhvv.os@mbbank.com.vn>
2026-04-02 19:32:59 +0700

    feat: allow public access to GET /banners endpoint in JwtTokenFilter

--- shopapp-backend/src/main/java/com/project/shopapp/filters/JwtTokenFilter.java
+++ shopapp-backend/src/main/java/com/project/shopapp/filters/JwtTokenFilter.java
@@ -77,6 +77,7 @@ public class JwtTokenFilter extends OncePerRequestFilter{
                 Pair.of(String.format("%s/products", apiPrefix), "GET"),
                 Pair.of(String.format("%s/orders", apiPrefix), "GET"),
                 Pair.of(String.format("%s/categories", apiPrefix), "GET"),
+                Pair.of(String.format("%s/banners", apiPrefix), "GET"),
                 Pair.of(String.format("%s/users/register", apiPrefix), "POST"),
                 Pair.of(String.format("%s/users/login", apiPrefix), "POST")
         );
